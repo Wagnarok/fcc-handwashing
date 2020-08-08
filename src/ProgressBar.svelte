feaa772a1cdda63e8583d22cdbf31f1f0385b43a
Wagnarok <jbenwagner@gmail.com>
2020-08-08 11:21:14 -0400

    timer code done

--- src/ProgressBar.svelte
+++ src/ProgressBar.svelte
@@ -1,3 +1,7 @@
+<script>
+  export let progress = 0;
+</script>
+
 <style>
   .progress-container {
     margin: 20px 0;
@@ -15,8 +19,8 @@
     src="handwashing.gif"
     alt="How to wash your hands"
     class="progress-container">
-    <div class="progress-bar" style="width: 20%">
-      <span class="sr-only">%</span>
+    <div class="progress-bar" style="width: {progress}%">
+      <span class="sr-only">%{progress}</span>
     </div>
   </div>
 </div>
