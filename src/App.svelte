f6eb8dc6af7b42dec1abe950f72aa1a9fda8abee
Wagnarok <jbenwagner@gmail.com>
2020-08-08 12:51:33 -0400

    sound working

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,10 @@
 <script>
   import Timer from './Timer.svelte';
   import HowTo from './HowTo.svelte';
+  let audio;
+  function timerEnds(e) {
+    audio.play();
+  }
 </script>
 
 <style>
@@ -11,7 +15,7 @@
 </style>
 
 <h1>Handwashing App</h1>
-<Timer />
+<Timer on:end={timerEnds} />
 
 <HowTo />
 
@@ -19,7 +23,12 @@
   <a href="https://www.who.int/gpsc/clean_hands_protection/en/">
     Picture Source
   </a>
-  <a href="https://freesound.org/people/metrostock99/sounds/345086/">
+  <a href="https://samplefocus.com/samples/chime-notification-alert">
     Sound Source
   </a>
 </h3>
+
+<audio bind:this={audio}>
+  <track kind="captions" />
+  <source src="alert.wav" />
+</audio>
--- src/Timer.svelte
+++ src/Timer.svelte
@@ -1,10 +1,13 @@
 <script>
+  import { createEventDispatcher } from 'svelte';
   import ProgressBar from './ProgressBar.svelte';
 
   const totalSeconds = 20;
   let secondsLeft = totalSeconds;
   $: progress = ((totalSeconds - secondsLeft) / totalSeconds) * 100;
   let isRunning = false;
+
+  const dispatch = createEventDispatcher();
   function startTimer() {
     isRunning = true;
     const timer = setInterval(() => {
@@ -13,6 +16,7 @@
         clearInterval(timer);
         isRunning = false;
         secondsLeft = totalSeconds;
+        dispatch('end');
       }
     }, 1000);
   }
